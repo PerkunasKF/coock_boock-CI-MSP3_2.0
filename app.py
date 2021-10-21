4008268180fab4a77d0b4c59fa26d34400e3f67a
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2021-10-21 10:19:55 +0000

    Add style.css and script.js files

--- app.py
+++ app.py
@@ -17,6 +17,13 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+@app.route("/")
+@app.route("/home")
+def home():
+    recipes = mongo.db.recipes.find()
+    return render_template("home.html", recipes=recipes)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+/* JQuery for Materialaiz */
+$(document).ready(function(){
+    $('.sidenav').sidenav({edge: "right"});
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+      });
+  });
